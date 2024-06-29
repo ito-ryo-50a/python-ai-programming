ab941745325beb2e16333be5aa28d555fe88dab6
ito-ryo-50a <1210riytoo@gmail.com>
2024-06-29 18:48:37 +0900

    edit

--- 3_3_3_2/main.py
+++ 3_3_3_2/main.py
@@ -23,7 +23,7 @@ plt.figure(figsize=(10, 6))
 sns.lineplot(data=df_melted, x='四半期', y='売上', hue='地区', style='部門')
 plt.title('部門別売上推移')
 plt.xticks(rotation=45)
-plt.savefig(os.path.join(output_folder, 'line_plot.png'))
+plt.savefig(f'{output_folder}/部門別売上推移.png')
 plt.close()
 
 # 棒グラフの作成
@@ -31,7 +31,7 @@ plt.figure(figsize=(10, 6))
 sns.barplot(data=df_melted, x='四半期', y='売上', hue='地区')
 plt.xticks(rotation=45)
 plt.title('地区別四半期売上')
-plt.savefig(os.path.join(output_dir, 'bar_plot.png'))
+plt.savefig(f'{output_folder}/地区別四半期売上.png')
 plt.close()
 
 # ヒートマップの作成
@@ -39,12 +39,12 @@ plt.figure(figsize=(10, 6))
 heatmap_data = df.drop(['地区', '部門'], axis=1).set_index(df['地区'] + ' ' + df['部門'])
 sns.heatmap(heatmap_data, annot=True, fmt='d', cmap='coolwarm')
 plt.title('部門別四半期別売上ヒートマップ')
-plt.savefig(os.path.join(output_dir, 'heatmap.png'))
+plt.savefig(f'{output_folder}/部門別四半期売上ヒートマップ.png')
 plt.close()
 
 # 箱ひげ図の作成
 plt.figure(figsize=(10, 6))
 sns.boxplot(data=df_melted, x='部門', y='売上', hue='地区')
 plt.title('各部門の売上分布')
-plt.savefig(os.path.join(output_dir, 'boxplot.png'))
+plt.savefig(f'{output_folder}/地区部門別売上分布.png')
 plt.close()
